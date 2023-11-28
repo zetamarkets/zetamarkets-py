9bd2dd34cea267420b3f6d3d655726652a8d5889
Filip <filip@sierra.team>
2023-11-28 16:21:05 +1100

    more culling

--- zetamarkets_py/zeta_client/types/__init__.py
+++ zetamarkets_py/zeta_client/types/__init__.py
@@ -4,9 +4,6 @@ from . import (
     anchor_decimal,
     asset,
     cross_margin_account_info,
-    expire_series_override_args,
-    expiry_series,
-    expiry_series_status,
     halt_args,
     halt_state,
     halt_state_args,
@@ -14,7 +11,6 @@ from . import (
     initialize_market_args,
     initialize_market_node_args,
     initialize_state_args,
-    initialize_zeta_group_args,
     initialize_zeta_pricing_args,
     kind,
     margin_account_type,
@@ -28,7 +24,6 @@ from . import (
     perp_parameters,
     place_order_type,
     position,
-    position_movement_arg,
     pricing_parameters,
     product,
     product_greeks,
@@ -38,14 +33,10 @@ from . import (
     strike,
     trait_type,
     treasury_movement_type,
-    update_greeks_args,
-    update_interest_rate_args,
     update_margin_parameters_args,
     update_perp_parameters_args,
     update_pricing_parameters_args,
     update_state_args,
-    update_volatility_args,
-    update_zeta_group_expiry_args,
     update_zeta_pricing_pubkeys_args,
     validation_type,
 )
@@ -55,12 +46,6 @@ from .cross_margin_account_info import (
     CrossMarginAccountInfo,
     CrossMarginAccountInfoJSON,
 )
-from .expire_series_override_args import (
-    ExpireSeriesOverrideArgs,
-    ExpireSeriesOverrideArgsJSON,
-)
-from .expiry_series import ExpirySeries, ExpirySeriesJSON
-from .expiry_series_status import ExpirySeriesStatusJSON, ExpirySeriesStatusKind
 from .halt_args import HaltArgs, HaltArgsJSON
 from .halt_state import HaltState, HaltStateJSON
 from .halt_state_args import HaltStateArgs, HaltStateArgsJSON
@@ -71,10 +56,6 @@ from .initialize_market_node_args import (
     InitializeMarketNodeArgsJSON,
 )
 from .initialize_state_args import InitializeStateArgs, InitializeStateArgsJSON
-from .initialize_zeta_group_args import (
-    InitializeZetaGroupArgs,
-    InitializeZetaGroupArgsJSON,
-)
 from .initialize_zeta_pricing_args import (
     InitializeZetaPricingArgs,
     InitializeZetaPricingArgsJSON,
@@ -87,11 +68,9 @@ from .movement_type import MovementTypeJSON, MovementTypeKind
 from .order_complete_type import OrderCompleteTypeJSON, OrderCompleteTypeKind
 from .order_state import OrderState, OrderStateJSON
 from .order_type import OrderTypeJSON, OrderTypeKind
-from .override_expiry_args import OverrideExpiryArgs, OverrideExpiryArgsJSON
 from .perp_parameters import PerpParameters, PerpParametersJSON
 from .place_order_type import PlaceOrderTypeJSON, PlaceOrderTypeKind
 from .position import Position, PositionJSON
-from .position_movement_arg import PositionMovementArg, PositionMovementArgJSON
 from .pricing_parameters import PricingParameters, PricingParametersJSON
 from .product import Product, ProductJSON
 from .product_greeks import ProductGreeks, ProductGreeksJSON
@@ -104,11 +83,6 @@ from .side import SideJSON, SideKind
 from .strike import Strike, StrikeJSON
 from .trait_type import TraitTypeJSON, TraitTypeKind
 from .treasury_movement_type import TreasuryMovementTypeJSON, TreasuryMovementTypeKind
-from .update_greeks_args import UpdateGreeksArgs, UpdateGreeksArgsJSON
-from .update_interest_rate_args import (
-    UpdateInterestRateArgs,
-    UpdateInterestRateArgsJSON,
-)
 from .update_margin_parameters_args import (
     UpdateMarginParametersArgs,
     UpdateMarginParametersArgsJSON,
@@ -122,11 +96,6 @@ from .update_pricing_parameters_args import (
     UpdatePricingParametersArgsJSON,
 )
 from .update_state_args import UpdateStateArgs, UpdateStateArgsJSON
-from .update_volatility_args import UpdateVolatilityArgs, UpdateVolatilityArgsJSON
-from .update_zeta_group_expiry_args import (
-    UpdateZetaGroupExpiryArgs,
-    UpdateZetaGroupExpiryArgsJSON,
-)
 from .update_zeta_pricing_pubkeys_args import (
     UpdateZetaPricingPubkeysArgs,
     UpdateZetaPricingPubkeysArgsJSON,
